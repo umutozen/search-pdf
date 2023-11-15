bb3fdcd620dc848e196fedae3f0e01ce3ba173f6
Umut Özen <writtenbyumut@gmail.com>
2023-11-15 12:16:03 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,26 +1,30 @@
-#Coded by Umut Özen
+# Coded By Umut Özen
+
 from googlesearch import search
 from rich import print
 import itertools
 
-def search_pdf_books(book_title, num_results=5):
+def search_pdf_books(book_title):
     query = f"{book_title} filetype:pdf"
 
     try:
         results = search(query)
-
+        
+        num_results = int(input("How many results do you want to see? "))
+        
         limited_results = itertools.islice(results, num_results)
 
         print("[bold cyan]Google Search Results (PDF Books):[/bold cyan]")
+        
         for index, result in enumerate(limited_results, 1):
+        
             print(f"[italic yellow]{index}. {result}[/italic yellow]")
 
     except Exception as e:
+    
         print("[bold red]An error occurred during the search:[/bold red]", str(e))
 
 if __name__ == "__main__":
-
     book_title = input("What book do you want to search for in PDF format on Google? ")
 
-    num_results = int(input("How many results do you want to see? "))
-    search_pdf_books(book_title, num_results)
+    search_pdf_books(book_title)
